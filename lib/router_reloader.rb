dbe89c06417bebb3435198110e4109fb97205c2e
bishboria <bishboria@gmail.com>
2014-06-30 09:46:48 +0100

    Revert "Enable router reloading in dev"

--- lib/router_reloader.rb
+++ lib/router_reloader.rb
@@ -9,14 +9,12 @@ class RouterReloader
     @router_reload_urls ||= YAML.load_file(Rails.root.join("config", "router_reload_urls.yml"))
   end
 
-  # To be set in dev mode so that this can run when the router isn't running.
-  cattr_accessor :swallow_connection_errors
-
   def initialize(urls)
     @urls = urls
   end
 
   def reload
+    return true unless should_reload?
     @errors = []
     @urls.each do |url|
       response = Net::HTTP.post_form(URI.parse(url), {})
@@ -30,8 +28,9 @@ class RouterReloader
       return false
     end
     true
-  rescue Errno::ECONNREFUSED
-    raise unless self.class.swallow_connection_errors
-    true
+  end
+
+  def should_reload?
+    ENV['ENABLE_ROUTER_RELOADING'].present?
   end
 end
